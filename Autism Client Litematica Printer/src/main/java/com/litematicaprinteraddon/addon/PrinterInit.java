1c3cf7493253c46fbeacb508ceeaa37026063d40
theflex5710 <devrimcavusoglu34@gmail.com>
2026-08-24 19:09:54 +0300

    Delete Autism Client Litematica Printer directory

--- Autism Client Litematica Printer/src/main/java/com/litematicaprinteraddon/addon/PrinterInit.java
+++
@@ -1,8 +0,0 @@
-package com.litematicaprinteraddon.addon;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public final class PrinterInit implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {}
-}
